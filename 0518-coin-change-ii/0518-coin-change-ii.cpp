090eca8f0a175fc0a78afe8bc8ac5553857b97d7
keshav3754 <keshavmittal053@gmail.com>
2026-07-25 15:03:12 +0530

    Time: 30 ms (52.25%), Space: 36.4 MB (65.94%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,27 +1,60 @@
+// class Solution {
+// public:
+//     int fn(vector<int>& coins, int idx, int amount, vector<vector<int>>& dp) {
+//         if (amount == 0)
+//             return 1;
+
+//         if (idx == 0) {
+//             return (amount % coins[0] == 0) ? 1 : 0;
+//         }
+
+//         if (dp[idx][amount] != -1)
+//             return dp[idx][amount];
+
+//         int npick = fn(coins, idx - 1, amount, dp);
+
+//         int pick = 0;
+//         if (coins[idx] <= amount) {
+//             pick = fn(coins, idx, amount - coins[idx], dp);
+//         }
+
+//         return dp[idx][amount] = pick + npick;
+//     }
+
+//     int change(int amount, vector<int>& coins) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+//         return fn(coins, n - 1, amount, dp);
+//     }
+// };
+
 class Solution {
 public:
-    int fn(vector<int>& coins, int idx, int amount, vector<vector<int>>& dp) {
-        if (amount == 0) return 1; 
-
-        if (idx == 0) {
-            return (amount % coins[0] == 0) ? 1 : 0;
-        }
-
-        if (dp[idx][amount] != -1) return dp[idx][amount];
-
-        int npick = fn(coins, idx - 1, amount, dp);
-
-        int pick = 0;
-        if (coins[idx] <= amount) {
-            pick = fn(coins, idx, amount - coins[idx], dp); 
-        }
-
-        return dp[idx][amount] = pick + npick;
-    }
-
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        return fn(coins, n - 1, amount, dp);
+
+        vector<vector<unsigned int>> dp(n, vector<unsigned int>(amount + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int j = 0; j <= amount; j++) {
+            dp[0][j] = (j % coins[0] == 0) ? 1 : 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                unsigned int npick = dp[i - 1][j];
+
+                unsigned int pick = 0;
+                if (coins[i] <= j) {
+                    pick = dp[i][j - coins[i]];
+                }
+
+                dp[i][j] = pick + npick;
+            }
+        }
+
+        return dp[n - 1][amount];
     }
 };
